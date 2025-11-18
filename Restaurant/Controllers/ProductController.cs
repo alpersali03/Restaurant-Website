8fe2a00cf26963c47a3189d76dfced8bf6bab8a1
Sigma98 <alsalilee@gmail.com>
2025-11-18 21:39:50 +0200

    ProductController updated

--- Restaurant/Controllers/ProductController.cs
+++ Restaurant/Controllers/ProductController.cs
@@ -1,26 +1,35 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Restaurant.Data;
 using Restaurant.Data.Models;
 using Restaurant.DTOs;
+using Restaurant.Services;
 
 namespace Restaurant.Controllers
 {
+	//[Authorize]
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext data;
         private readonly IMapper _mapper;	
+		private readonly ICategoryService _categoryService;
+        private readonly IProductService _productService;
 
-        public ProductController(ApplicationDbContext data, IMapper mapper)
+        public ProductController(ApplicationDbContext data, IMapper mapper, ICategoryService categoryService, IProductService productService)
         {
             this.data = data;
             this._mapper = mapper;
+			this._categoryService = categoryService;
+            this._productService = productService;
         }
 		[HttpGet]
 		public IActionResult Add()
 		{
 			ProductFormDto productFormDto = new ProductFormDto();
-			productFormDto.Categories = _mapper.Map<List<CategoryFormDto>>(data.Categories.ToList());
+            productFormDto.Categories = _categoryService.GetAll();
 			return View(productFormDto);
 
 		}
@@ -31,59 +40,71 @@ namespace Restaurant.Controllers
 			{
 				throw new Exception("The category already exists");
 			}
-			Product mapped = _mapper.Map<Product>(productFormDto);
 
-			this.data.Add(mapped);
-			data.SaveChanges();
+			_productService.Add(productFormDto);
 			return RedirectToAction("GetAll");
 		}
 		[HttpGet]
         public IActionResult Getall()
         {
-            var products = data.Products.ToList();
-            var productDto = _mapper.Map<List<ProductFormDto>>(products);
+            var products = _productService.GetAll();
+            
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = this.data.Products.FirstOrDefault(p => p.Id == id);
+            
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            var productDto = _mapper.Map<ProductFormDto>(product);
+            productDto.Categories = _categoryService.GetAll();
+
             return View(productDto);
         }
+        [HttpPost]
+        public IActionResult Edit(ProductFormDto product)
+        {
+            var existingProduct = this.data.Products.FirstOrDefault(c => c.Id == product.Id);
+
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                
+                product.Categories = _categoryService.GetAll();
+                return View(product);
+            }
+
+            existingProduct.Name = product.Name;
+            existingProduct.Description = product.Description;
+            existingProduct.ImageUrl = product.ImageUrl;
+            existingProduct.Price = product.Price;
+            existingProduct.IsAvailable = product.IsAvailable;
+            existingProduct.CategoryId = product.CategoryId;
+
+            data.SaveChanges();
+            return RedirectToAction("GetAll");
+        }
 		[HttpGet]
-		public IActionResult Edit(int id)
+		public IActionResult Details(int id)
 		{
-			var product = this.data.Products.FirstOrDefault(p => p.Id == id);
-			if (product == null)
-			{
-				return NotFound();
-			}
-			var productDto = _mapper.Map<ProductFormDto>(product);
-			return View(productDto);
+            var product = _productService.GetDetails(id);
+			return View(product);
 		}
-		[HttpPost]
-		public IActionResult Edit(ProductFormDto product)
-		{
-			var existingProduct = this.data.Products.FirstOrDefault(c => c.Id == product.Id);
 
-			if (existingProduct == null)
-			{
-				return NotFound();
-			}
-
-			existingProduct.Name = product.Name;
-			//existingProduct.Category = product.Categories;
-			existingProduct.ImageUrl = product.ImageUrl;
-			existingProduct.Price = product.Price;
-			data.SaveChanges();
-			return RedirectToAction("GetAll");
-
-		}
 		[HttpGet]
 		public IActionResult Delete(int id)
 		{
-			var deletingProduct = this.data.Products.FirstOrDefault(x => x.Id == id);
-			if (deletingProduct == null)
-			{
-				return NotFound();
-			}
-			this.data.Remove(deletingProduct);
-			data.SaveChanges();
-			return RedirectToAction("getall");
+			_productService.Delete(id);
+            return RedirectToAction("getall");
 
 		}
 
